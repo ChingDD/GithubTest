e2bfcdc6e5118d8d15c3b3060127272557a82014
tamatomato <love4511362@gmail.com>
2023-06-12 09:49:30 +0800

    add print

--- GithubTest/ViewController.swift
+++ GithubTest/ViewController.swift
@@ -13,8 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .blue
+        print("123132123")
     }
 
+    
 
 }
 
